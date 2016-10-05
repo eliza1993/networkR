7ccceb467eb6ae411df570bdd52a8c2bd2890459
lu446529842 <446529842@qq.com>
2016-10-05 13:12:17 +0800

    mysqlConnector

--- networkR/networkR.py
+++ networkR/networkR.py
@@ -6,19 +6,36 @@ from networkR.dao.SiteGrabHistory import SiteGrabHistory
 from networkR.dao.mysqlConnector import mysqlConnector
 
 
-if __name__ == '__main__':
-	url = 'https://www.baidu.com'
-	url2 = 'http://www.baidu.com'
 
+def process_item_test():
 	mysqlConn = mysqlConnector()
 	dbConn = mysqlConn.openDb('172.16.111.87','root','','Spider')
 	siteGbHis = SiteGrabHistory(dbConn)
 	pipeline = NetworkrPipeline(siteGbHis)
-	print pipeline.get_domain(url)
-	print pipeline.get_domain(url2)
-
 	items = {}
 	pipeline.process_item(items,None)
+
+	mysqlConn.close(dbConn)
+
+def get_domain_test():
+	url = 'https://www.baidu.com'
+	url2 = 'http://www.baidu.com'
+	pipeline = NetworkrPipeline(None)
+	print pipeline.get_domain(url)
+	print pipeline.get_domain(url2)
+	
+
+if __name__ == '__main__':
+	get_domain_test()
+
+	process_item_test()
+
+
+
+
+	
+
+
 
 
 
