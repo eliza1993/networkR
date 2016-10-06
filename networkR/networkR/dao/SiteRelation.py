9762e27fd4e91798939e9164048b63c04f82c6eb
lu446529842 <446529842@qq.com>
2016-10-06 15:40:36 +0800

    提交代码

--- networkR/networkR/dao/SiteRelation.py
+++ networkR/networkR/dao/SiteRelation.py
@@ -1,4 +1,5 @@
-import mysqlConnector
+from mysqlConnector import mysqlConnector
+import datetime
 
 class SiteRelation(object):
 	"""docstring for SiteRelation"""
@@ -7,10 +8,10 @@ class SiteRelation(object):
 		
 
 	def get_cursor(self):
-		return mysqlConn.cursor()
+		return self.mysqlConn.cursor()
 
 	def insert_one(self,items = []):
-		insert_sql = "insert into SiteRelation(masterSite,outLinkSite,outLinkCount,createTime,lastUpdateTime) values(%s,%s,%d,%s,%s)"
+		insert_sql = "insert into SiteRelation(masterSite,outLinkSite,outLinkCount,createTime,lastUpdateTime) values(%s,%s,%s,%s,%s)"
 		insert_item  = []
 		insert_item.append(items['masterSite'])
 		insert_item.append(items['outLinkSite'])
@@ -19,30 +20,61 @@ class SiteRelation(object):
 		insert_item.append(items['lastUpdateTime'])
 		cursor = self.get_cursor()
 		cursor.execute(insert_sql,insert_item)
-		cursor.commit()
+		self.mysqlConn.commit()
 
 
 	def has_site_relation(self,items = {}):
-		query_sql = "select * from SiteRelation where masterSite = '%s' and outLinkSite = '%s'"
+		query_sql = "select * from SiteRelation where masterSite = '%s' and outLinkSite = '%s'" %(items['masterSite'],items['outLinkSite'])
 		cursor = self.get_cursor()
 		cursor.execute(query_sql)
-		results = cursor.fetchAll()
+		results = cursor.fetchall()
 		return len(results) > 0
 
-	def function(self,items = {}):
-		query_sql = "select * from SiteRelation where masterSite = '%s' and outLinkSite = '%s'"
+	def increase_one(self,items = {}):
+		query_sql = "select outLinkCount from SiteRelation where masterSite = '%s' and outLinkSite = '%s'" %(items['masterSite'],items['outLinkSite'])
 		cursor = self.get_cursor()
 		cursor.execute(query_sql)
-		results = cursor.fetchAll()
+		results = cursor.fetchall()
+		print results
 		if len(results) > 0:
 			result = results[0]
-			count = result['outLinkCount'] +1
-			update_sql = "update SiteRelation set outLinkCount = %d where masterSite = '%s' and outLinkSite = '%s'"
+			count = result[0] + 1
+			update_sql = "update SiteRelation set outLinkCount = %s where masterSite = '%s' and outLinkSite = '%s'" %(count,items['masterSite'],items['outLinkSite'])
 			cursor.execute(update_sql)
-			cursor.commit()
+			self.mysqlConn.commit()
 
-	def query(self,items = []):
-		pass
 
-	def update(self,items = []):
-		pass
+
+def test_insert_one(siteRel):
+	items = {}
+	items['masterSite'] = 'www.baidu.com'
+	items['outLinkSite'] = 'www.tongdun.com'
+	items['outLinkCount'] = 100
+	items['createTime'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
+	items['lastUpdateTime'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
+	siteRel.insert_one(items)
+	
+
+def test_has_site_relation(siteRel):
+	items = {}
+	items['masterSite'] = 'www.baidu.com'
+	items['outLinkSite'] = 'www.tongdun.com'
+	print siteRel.has_site_relation(items)
+
+
+def test_increase_one(siteRel):
+	items = {}
+	items['masterSite'] = 'www.baidu.com'
+	items['outLinkSite'] = 'www.tongdun.com'
+	siteRel.increase_one(items)
+	
+	
+
+if __name__ == '__main__':
+	link = mysqlConnector()
+	connect = link.openDb('172.16.111.87','root','','Spider')
+	siteRel = SiteRelation(connect)
+
+	#test_insert_one(siteRel)
+	test_has_site_relation(siteRel)
+	test_increase_one(siteRel)
