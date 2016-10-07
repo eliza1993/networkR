0f5c2a07bd468806fb4ee8b0a59825c87ba8431c
lu446529842 <446529842@qq.com>
2016-10-07 21:09:25 +0800

    urls

--- networkR/networkR/util/UrlUtil.py
+++ networkR/networkR/util/UrlUtil.py
@@ -1,17 +1,21 @@
 
 
 def get_domain(url):
-	# if 'http://' in url:
-	# 	url = url[7:]
+    http_pro = ''
+    if 'http://' in url:
+        http_pro = 'http://'
+        url = url[7:]
 
-	# if 'https://' in url:
-	# 	url = url[8:]
+    if 'https://' in url:
+        http_pro = 'https://'
+        url = url[8:]
 
-	if '/' in url:
-		index = url.index('/')
-		url = url[0:index]
+    if '/' in url:
+        index = url.index('/')
+        url = url[0:index]
 
-	return url
+    url = http_pro + url
+    return url
 
 
 
