646770ad8eb35622951a752d1f26a5b7e40d01bf
lu446529842 <446529842@qq.com>
2016-10-07 21:05:06 +0800

    urls

--- networkR/networkR/pipelines.py
+++ networkR/networkR/pipelines.py
@@ -101,16 +101,20 @@ class NetworkrPipeline(object):
 
 
 	def get_domain(self,url):
-		# if 'http://' in url:
-		# 	url = url[7:]
+		http_pro = ''
+		if 'http://' in url:
+			http_pro = 'http://'
+			url = url[7:]
 
-		# if 'https://' in url:
-		# 	url = url[8:]
+		if 'https://' in url:
+			http_pro = 'https://'
+			url = url[8:]
 
 		if '/' in url:
 			index = url.index('/')
 			url = url[0:index]
 
+		url = http_pro + url
 		return url
 
 
