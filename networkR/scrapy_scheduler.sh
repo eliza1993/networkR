93554765beb3cd434111bdf577b916cc5427bb41
lu446529842 <446529842@qq.com>
2016-10-07 21:58:05 +0800

    add cron sh

--- networkR/scrapy_scheduler.sh
+++ networkR/scrapy_scheduler.sh
@@ -1,9 +1,9 @@
 #!/bin/bash/                                                                                                                                  
 
 #cron 表达式 每5分钟执行 一次
-# */1 * * * *  sh /home/yuzhe/git/networkR/scrapy_scheduler.sh
+# */5 * * * *  sh /home/yuzhe/git/networkR/scrapy_scheduler.sh
 export PATH=$PATH:/usr/local/bin
 
 cd /home/yuzhe/git/networkR
 
-nohup scrapy crawl networkr >> networkr.log 2>&1 &
+nohup scrapy crawl networkr >> /home/yuzhe/git/networkR/networkr.log 2>&1 &
